53615b091e12c91820755cc8ad3f02abee0463f9
Natalia <nannimagi@gmail.com>
2025-10-16 17:50:44 +0200

    submit

--- src/components/Forms/UserForm/UserForm.js
+++ src/components/Forms/UserForm/UserForm.js
@@ -3,15 +3,16 @@ import './UserForm.css';
 export const UserForm = (parentElement, formName, fields) => {
   const form = document.createElement('form');
   form.classList.add('flex-container', 'user-form');
+
   const title = document.createElement('h2');
   title.textContent = formName;
   form.append(title);
-for (const field of fields) {
+
+  for (const field of fields) {
   const inputContainer = document.createElement('div');
   inputContainer.classList.add('input-container');
 
   let input;
-
   if (field.type === 'textarea') {
     input = document.createElement('textarea');
   } else if (field.type === 'select') {
@@ -32,6 +33,7 @@ for (const field of fields) {
   input.classList.add('input');
   input.id = field.id;
   input.name = field.name || field.id;
+
   const label = document.createElement('label');
   label.classList.add('iLabel');
   label.htmlFor = field.id;
@@ -40,4 +42,11 @@ for (const field of fields) {
   inputContainer.append(input, label);
   form.append(inputContainer);
 }
+const submitButton = document.createElement('button');
+  submitButton.type = 'submit';
+  submitButton.textContent = 'Submit';
+  submitButton.classList.add('submit');
+  form.append(submitButton);
+
+  parentElement.append(form);
 };
